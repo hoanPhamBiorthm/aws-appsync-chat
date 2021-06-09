27a2ee120fc462111f1ecf35cdb7d17f71cccbb6
Hoan <hoan@bio-rithm.com>
2021-06-09 14:13:11 +0700

    Use compose by lodash

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { css } from 'glamor'
 import { observer } from 'mobx-react'
-import { graphql, compose } from 'react-apollo'
+import { graphql } from 'react-apollo'
+import {flowRight as compose} from 'lodash';
 import { FaComments, FaChevronRight } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
